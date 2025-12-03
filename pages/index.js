9761d8b2fc2aff19176013833eb4a5baeb80b1cf
Rafael Degolin da Silva <103646841+Rafadegolin@users.noreply.github.com>
2025-12-03 17:58:11 +0000

    adiciona scripts

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,12 @@ function Home() {
   return <h1>Carol eu te amo MUITO, se você me ama da uma risadinha!</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
+}
+
 export default Home;
